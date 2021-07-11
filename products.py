4fc9e97f98323cb5e63ed2a0560ce11abdc0088c
Jojo Lin <jkggy3618@gmail.com>
2021-07-11 22:54:03 +0800

    add check file the list

--- products.py
+++ products.py
@@ -1,15 +1,23 @@
+import os # operating system
+
 #讀取檔案
 products = []
+if os.path.isfile('products.csv'):#os的路徑使用isfile功能 檢查檔在不在
+	print('Yeah!找到檔案')
+	with open ('products.csv','r',encoding='utf-8') as f:
+		for line in f:
+			if'商品,價格' in line:
+				continue # 繼續 如果遇到商品或價格在line裡面就跳到下一回
+				name, price=line.strip().split(',')#用strip 去掉\n 再用split 切割完的結果是清單
+				#name = s[0]
+				#price = s[1]
+				products.append([name,price])
+	print(products)
 
-with open ('products.csv','r',encoding='utf-8') as f:
-	for line in f:
-		if'商品,價格' in line:
-			continue # 繼續 如果遇到商品或價格在line裡面就跳到下一回
-		name, price=line.strip().split(',')#用strip 去掉\n 再用split 切割完的結果是清單
-		#name = s[0]
-		#price = s[1]
-		products.append([name,price])
-print(products)
+
+else:
+	print('找不到檔案')
+
 
 #讓使用者輸入
 while True:
